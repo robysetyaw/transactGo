673ad6e17356dc3dd940f431fd21ab3408755704
robysetyaw <robysetyaw@gmail.com>
2023-08-01 12:53:09 +0700

    add user_repository

---
+++ app/repository/user_repository.go
@@ -0,0 +1,95 @@
+package repository
+
+import (
+	"encoding/json"
+	"os"
+	"transactgo/app/model"
+)
+
+type UserRepository interface {
+	FindByUsername(username string) *model.User
+	Save(user *model.User) error
+	Delete(user *model.User) error
+}
+
+type userRepository struct {
+	users []model.User
+}
+
+func NewUserRepository() (UserRepository, error) {
+	repo := &userRepository{}
+
+	// Open the JSON file
+	file, err := os.Open("data/users.json")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Decode the file into the users slice
+	err = json.NewDecoder(file).Decode(&repo.users)
+	if err != nil {
+		return nil, err
+	}
+
+	return repo, nil
+}
+
+func (r *userRepository) FindByUsername(username string) *model.User {
+	for _, user := range r.users {
+		if user.Username == username {
+			return &user
+		}
+	}
+	return nil
+}
+
+func (r *userRepository) Save(user *model.User) error {
+	// Update the user in the slice
+	for i, u := range r.users {
+		if u.ID == user.ID {
+			r.users[i] = *user
+			break
+		}
+	}
+
+	// Open the JSON file
+	file, err := os.OpenFile("data/users.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Encode the users slice back into the file
+	err = json.NewEncoder(file).Encode(r.users)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *userRepository) Delete(user *model.User) error {
+	// Remove the user from the slice
+	for i, u := range r.users {
+		if u.ID == user.ID {
+			r.users = append(r.users[:i], r.users[i+1:]...)
+			break
+		}
+	}
+
+	// Open the JSON file
+	file, err := os.OpenFile("data/users.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Encode the users slice back into the file
+	err = json.NewEncoder(file).Encode(r.users)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
