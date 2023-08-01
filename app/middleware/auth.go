2866c1cc22b769cfe8e375c4aeaa8ff12fdb2788
robysetyaw <robysetyaw@gmail.com>
2023-08-01 18:36:04 +0700

    add auth middleware JWT

---
+++ app/middleware/auth.go
@@ -0,0 +1,58 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Authorization header is missing",
+			})
+			c.Abort()
+			return
+		}
+
+		bearerToken := strings.Split(authHeader, " ")
+		if len(bearerToken) != 2 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid token format",
+			})
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+			// Add validation key logic here, for instance fetching your secret key from environment variable
+			// return []byte("YourSecretKey"), nil
+
+			// We're not adding the validation logic here for simplicity
+			return nil, nil
+		})
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid token",
+			})
+			c.Abort()
+			return
+		}
+
+		if !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid token",
+			})
+			c.Abort()
+			return
+		}
+
+		// Token is valid
+		c.Next()
+	}
+}
