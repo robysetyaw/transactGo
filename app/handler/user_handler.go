cdad6519cbe46d8b8c2acc473c95cdf828a8785f
robysetyaw <robysetyaw@gmail.com>
2023-08-01 13:15:23 +0700

    add update & delete user in user_handler

--- app/handler/user_handler.go
+++ app/handler/user_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"transactgo/app/model"
 	"transactgo/app/service"
 
 	"github.com/gin-gonic/gin"
@@ -16,8 +17,8 @@ func NewUserHandler(s *service.UserService, r *gin.Engine) *UserHandler {
 
 	// Set up routes
 	r.GET("/users/:username", handler.GetUserByUsername)
-	// r.PUT("/users/:username", handler.UpdateUser)
-	// r.DELETE("/users/:username", handler.DeleteUser)
+	r.PUT("/users/:username", handler.UpdateUser)
+	r.DELETE("/users/:username", handler.DeleteUser)
 
 	return handler
 }
@@ -30,4 +31,33 @@ func (h *UserHandler) GetUserByUsername(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, user)
+}
+
+func (h *UserHandler) UpdateUser(c *gin.Context) {
+	username := c.Param("username")
+	var user model.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user.Username = username
+	if err := h.service.UpdateUser(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (h *UserHandler) DeleteUser(c *gin.Context) {
+	username := c.Param("username")
+	user := h.service.GetUserByUsername(username)
+	if user == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	if err := h.service.DeleteUser(user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete user"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "User deleted"})
 }
