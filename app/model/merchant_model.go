0418bec1013d74c1f37b22eb2056087011b674f0
robysetyaw <robysetyaw@gmail.com>
2023-08-02 14:51:10 +0700

    update merchants model

--- app/model/merchant_model.go
+++ app/model/merchant_model.go
@@ -1,6 +1,9 @@
 package model
 
 type Merchant struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	UserID       string `json:"userId"`
+	BusinessName string `json:"businessName"`
+	BusinessType string `json:"businessType"`
 }
