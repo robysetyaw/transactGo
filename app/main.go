a79802aeb22bdc1348f1d44a269d7e8fa0713235
robysetyaw <robysetyaw@gmail.com>
2023-08-02 15:23:24 +0700

    add merchants config at main.go

--- app/main.go
+++ app/main.go
@@ -12,19 +12,19 @@ func main() {
 	// Set up Gin server
 	r := gin.Default()
 	// Create UserRepository
-	userRepo, err := repository.NewUserRepository()
-	if err != nil {
-		panic(err)
-	}
+	userRepo, _ := repository.NewUserRepository()
 	accountRepo , _ := repository.NewAccountRepository()
+	merchantRepo, _ := repository.NewMerchantRepository()
 
 	// Create UserService
 	userService := service.NewUserService(userRepo)
 	accountService := service.NewAccountService(accountRepo)
+	merchantService := service.NewMerchantService(merchantRepo)
 
 	// Create UserHandler
 	handler.NewUserHandler(userService, r)
 	handler.NewAccountHandler(accountService, r)
+	handler.NewMerchantHandler(merchantService, r)
 	
 	// Start the server
 	r.Run() // listen and serve on 0.0.0.0:8080
