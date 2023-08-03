9d406f2d15c867526690dfe9be6e17406b38b83d
robysetyaw <robysetyaw@gmail.com>
2023-08-03 09:40:33 +0700

    delete payment.go

--- app/model/payment_model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Payment struct {
-	ID         int    `json:"id"`
-	UserID     int    `json:"userId"`
-	MerchantID int    `json:"merchantId"`
-	Amount     int    `json:"amount"`
-	Timestamp  string `json:"timestamp"`
-}
