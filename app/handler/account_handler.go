951d7d3c5b75bdaa1f2bdf0cb1e966ba2f40cac4
robysetyaw <robysetyaw@gmail.com>
2023-08-03 10:27:54 +0700

    fix accountHandler response

--- app/handler/account_handler.go
+++ app/handler/account_handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"transactgo/app/middleware"
 	"transactgo/app/model"
+	"transactgo/app/model/response"
 	"transactgo/app/service"
 
 	"github.com/gin-gonic/gin"
@@ -27,59 +28,58 @@ func NewAccountHandler(accountService service.AccountService, r *gin.Engine) *Ac
 
 func (h *AccountHandler) GetActiveAccounts(c *gin.Context) {
 	activeAccounts := h.accountService.FindAllActive()
-	c.JSON(http.StatusOK, activeAccounts)
+	c.JSON(http.StatusOK, response.NewResponse(http.StatusOK, "OK","Successfully get active accounts", activeAccounts, " "))
 }
 
 func (h *AccountHandler) GetAccount(c *gin.Context) {
 	accountNumber := c.Param("accountNumber")
 	account, err := h.accountService.FindByAccountNumber(accountNumber)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		c.JSON(http.StatusNotFound, response.NewResponse(http.StatusNotFound, "Failed", err.Error(), nil, ""))
 		return
 	}
-	c.JSON(http.StatusOK, account)
+	c.JSON(http.StatusOK, response.NewResponse(http.StatusOK, "OK","Successfully get account", account, " "))
 }
 
 func (h *AccountHandler) CreateAccount(c *gin.Context) {
 	var newAccount model.Account
 	if err := c.ShouldBindJSON(&newAccount); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, response.NewResponse(http.StatusBadRequest, "Invalid input", err.Error(), nil, ""))
 		return
 	}
 	user, exists := c.Get("username")
 	if !exists {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get user"})
+		c.JSON(http.StatusInternalServerError, response.NewResponse(http.StatusInternalServerError, "Failed", "Failed to get user", nil, ""))
 		return
 	}
 	userString := user.(string)
 	h.accountService.Save(&newAccount,userString)
-	c.JSON(http.StatusCreated, newAccount)
+	c.JSON(http.StatusCreated, response.NewResponse(http.StatusCreated, "OK","Successfully created account", newAccount, " "))
 }
 
 func (h *AccountHandler) UpdateAccount(c *gin.Context) {
-	// accountNumber := c.Param("accountNumber")
 	var updatedAccount model.Account
 	if err := c.ShouldBindJSON(&updatedAccount); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, response.NewResponse(http.StatusBadRequest, "Invalid input", err.Error(), nil, ""))
 		return
 	}
 	if err := h.accountService.Update(&updatedAccount); err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		c.JSON(http.StatusNotFound, response.NewResponse(http.StatusNotFound, "Failed", err.Error(), nil, ""))
 		return
 	}
-	c.JSON(http.StatusOK, updatedAccount)
+	c.JSON(http.StatusOK, response.NewResponse(http.StatusOK, "OK","Successfully updated account", updatedAccount, " "))
 }
 
 func (h *AccountHandler) DeleteAccount(c *gin.Context) {
 	accountNumber := c.Param("accountNumber")
 	account, err := h.accountService.FindByAccountNumber(accountNumber)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		c.JSON(http.StatusNotFound, response.NewResponse(http.StatusNotFound, "Failed", err.Error(), nil, ""))
 		return
 	}
 	if err := h.accountService.DeActive(account); err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		c.JSON(http.StatusNotFound, response.NewResponse(http.StatusNotFound, "Failed", err.Error(), nil, ""))
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "Account deleted successfully"})
+	c.JSON(http.StatusOK, response.NewResponse(http.StatusOK, "OK","Account deleted successfully", nil, " "))
 }
