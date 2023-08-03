109e5fc093d959d55e56a17aee6485c6f6b38717
robysetyaw <robysetyaw@gmail.com>
2023-08-03 10:29:09 +0700

    fix txHandler response

--- app/handler/transaction_handler.go
+++ app/handler/transaction_handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"transactgo/app/middleware"
 	"transactgo/app/model"
+	"transactgo/app/model/response"
 	"transactgo/app/service"
 
 	"github.com/gin-gonic/gin"
@@ -27,41 +28,41 @@ func NewTransactionHandler(s service.TransactionService, r *gin.Engine) *Transac
 func (h *TransactionHandler) GetTransactions(c *gin.Context) {
 	txs, err := h.service.GetTransactions()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get transactions"})
+		c.JSON(http.StatusInternalServerError, response.NewResponse(http.StatusInternalServerError, "Failed", "Failed to get transactions", nil, err.Error()))
 		return
 	}
-	c.JSON(http.StatusOK, txs)
+	c.JSON(http.StatusOK, response.NewResponse(http.StatusOK, "OK","Successfully get transactions", txs, ""))
 }
 
 func (h *TransactionHandler) GetTransaction(c *gin.Context) {
 	id := c.Param("id")
 	tx, err := h.service.GetTransaction(id)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Transaction not found"})
+		c.JSON(http.StatusNotFound, response.NewResponse(http.StatusNotFound, "Failed", "Transaction not found", nil, err.Error()))
 		return
 	}
-	c.JSON(http.StatusOK, tx)
+	c.JSON(http.StatusOK, response.NewResponse(http.StatusOK, "OK","Successfully get transaction", tx, ""))
 }
 
 func (h *TransactionHandler) CreateTransaction(c *gin.Context) {
 	var tx model.Transaction
 	if err := c.ShouldBindJSON(&tx); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, response.NewResponse(http.StatusBadRequest, "Invalid input", err.Error(), nil, ""))
 		return
 	}
 
 	user, exists := c.Get("username")
 
 	if !exists {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get user"})
+		c.JSON(http.StatusInternalServerError, response.NewResponse(http.StatusInternalServerError, "Failed", "Failed to get user", nil, ""))
 		return
 	}
 
 	data, err := h.service.CreateTransaction(tx, user)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create transaction"})
+		c.JSON(http.StatusInternalServerError, response.NewResponse(http.StatusInternalServerError, "Failed", "Failed to create transaction", nil, err.Error()))
 		return
 	}
 
-	c.JSON(http.StatusCreated, data)
+	c.JSON(http.StatusCreated, response.NewResponse(http.StatusCreated, "OK","Successfully created transaction", data, ""))
 }
